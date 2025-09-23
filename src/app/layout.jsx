083cd0fb571925a53045d5c4e33f5e950ac7c133
mrakasondara <rakasondara21@gmail.com>
2025-09-23 20:00:02 +0700

    nextjs initial commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,23 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const metadata = {
+  title: {
+    template: "%s | Finance App",
+    default: "Finance App",
+  },
+  description: "A Simple Finance App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
